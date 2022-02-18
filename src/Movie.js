ded3f3a77e7877cb705a6ddd4d7c7a92622f8076
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-02-18 10:38:23 -0800

    movie search working!

--- src/Movie.js
+++ src/Movie.js
@@ -1,7 +1,31 @@
+import { addToList } from './services/fetch-utils';
+
+export default function Movie({ movie, onList, reloadSavedList }) {
+  const watched = onList(movie.id);
+
+  async function handleClick() {
+    if (!watched) {
+      const savedListMovie = {
+        title: movie.title,
+        api_id: movie.id,
+        description: movie.overview,
+        poster: movie.poster_path
+      };
+
+      await addToList(savedListMovie);
+      await reloadSavedList();
+    }
+  }
 
 
-export default function Movie() {
   return (
-    <div>Movie</div>
+    <div 
+      onClick={handleClick}
+      className={`movie ${watched ? 'watched' : ''}`}>
+      
+      <p>{watched && 'watched'}</p>
+      <p>{movie.title}</p>
+
+    </div>
   );
 }
