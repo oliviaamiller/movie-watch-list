2f053c03773ebbe6b26433c4d10e21e7a6dbb057
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-02-18 09:48:24 -0800

    mapping through movies

--- src/ListPage.js
+++ src/ListPage.js
@@ -1,10 +1,22 @@
-// import useLocation
-// import movie.js
-// import savedlistitem.js
+import { useLocation } from 'react-router-dom';
+import Movie from './Movie';
+import SavedListMovie from './SavedListMovie';
 
 
 export default function ListPage({ movies, onList, reloadSavedList }) {
+  const location = useLocation();
+
   return (
-    <div>ListPage</div>
+    <div className='movie-search-results'>
+      {movies.map((movie, i) => location.pathname.includes('search')
+        ? <Movie key={`${movie.title}-${i}`}
+          movie={movie}
+          onList={onList}
+          reloadSavedList={reloadSavedList} />
+        : <SavedListMovie key={`${movie.title}-${i}`}
+          movie={movie}
+          reloadSavedList={reloadSavedList} />)
+      }
+    </div>
   );
 }
---
+++ src/SavedListMovie.js
@@ -0,0 +1,7 @@
+
+
+export default function SavedListMovie() {
+  return (
+    <div>SavedListMovie</div>
+  );
+}
